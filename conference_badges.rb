f33730e0291775bd45a9b503104553f58af24ab0
Brittany Hartmire <bkhartmire@gmail.com>
2017-11-18 13:52:44 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,3 +2,11 @@
 def badge_maker(name)
   puts "Hello, my name is #{name}."
 end
+
+names = [ "Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def batch_badge_creator(names)
+  names.each do |name|
+    badge_maker(name)
+  end
+end
